fb6bac42007efbc59ec878873444920a870a7765
David Hartong <davidahartong@gmail.com>
2024-08-17 18:44:18 +0800

    Added README.md

--- main_ma.py
+++ main_ma.py
@@ -1,9 +1,11 @@
 import random
 import sys
+from copy import copy
 
 import pygame
 from pettingzoo.utils import parallel_to_aec
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import StopTrainingOnNoModelImprovement, EvalCallback
 from stable_baselines3.ppo import MlpPolicy
 
 import supersuit as ss
@@ -11,9 +13,20 @@ import supersuit as ss
 from src.ma_environment import MultiAgentEnvironment
 
 
-def train(env_fn, steps: int = 100_000, seed: int = None, **env_kwargs):
-    env = env_fn(**env_kwargs)
+def train(env_fn, steps_single: int = 100_000, steps_multi: int = 100_000, seed: int = None, **env_kwargs):
+    width = env_kwargs.get("width")
+    height = env_kwargs.get("height")
+    num_agents_single = env_kwargs.get("num_aircraft_single")
+    num_agents_multi = env_kwargs.get("num_aircraft_multi")
 
+    env_kwargs_single = {'width': width,
+                         'height': height,
+                         'num_aircraft': num_agents_single}
+    env_kwargs_multi = {'width': width,
+                        'height': height,
+                        'num_aircraft': num_agents_multi}
+
+    env = env_fn(**env_kwargs_single)
     env = ss.black_death_v3(env)
     env.reset(seed=seed)
 
@@ -22,17 +35,34 @@ def train(env_fn, steps: int = 100_000, seed: int = None, **env_kwargs):
     env = ss.pettingzoo_env_to_vec_env_v1(env)
     env = ss.concat_vec_envs_v1(env, 1, base_class='stable_baselines3')
 
+    eval_env = copy(env)
+
+    stop_train_callback = StopTrainingOnNoModelImprovement(max_no_improvement_evals=10, min_evals=50, verbose=1)
+    eval_callback = EvalCallback(eval_env, eval_freq=10240, callback_after_eval=stop_train_callback, verbose=1)
+
     model = PPO(
         MlpPolicy,
         env,
         verbose=1,
         learning_rate=0.0003,
-        n_steps=1024,
+        n_steps=2048,
+        n_epochs=20,
         gamma=0.999,
         tensorboard_log="logs"
     )
 
-    model.learn(total_timesteps=steps)
+    model.learn(total_timesteps=steps_single, callback=eval_callback)
+    model.save('ppo_model_single')
+    env = env_fn(**env_kwargs_multi)
+    env = ss.black_death_v3(env)
+    env.reset(seed=seed)
+
+    print(f"Starting multi-agent training on {str(env.metadata['name'])}.")
+
+    env = ss.pettingzoo_env_to_vec_env_v1(env)
+    env = ss.concat_vec_envs_v1(env, 1, base_class='stable_baselines3')
+
+    model.learn(total_timesteps=steps_multi)
 
     model.save("ppo_model")
 
@@ -40,6 +70,9 @@ def train(env_fn, steps: int = 100_000, seed: int = None, **env_kwargs):
 
 
 def eval(env_fn, num_games: int = 100, render_mode: str = None, **env_kwargs):
+    env_kwargs = {'width': env_kwargs.get('width'),
+                  'height': env_kwargs.get('height'),
+                  'num_aircraft': env_kwargs.get('num_aircraft_multi')}
     env = env_fn(render_mode=render_mode, **env_kwargs)
 
     env = parallel_to_aec(env)
@@ -93,7 +126,10 @@ def eval(env_fn, num_games: int = 100, render_mode: str = None, **env_kwargs):
 
 
 if __name__ == "__main__":
-    env_kwargs = {'width': 400, 'height': 400, 'num_aircraft': 1}
-    # train(MultiAgentEnvironment, steps=3_000_000, **env_kwargs)
+    env_kwargs = {'width': 400, 'height': 400, 'num_aircraft_single': 1, 'num_aircraft_multi': 10}
+    train(MultiAgentEnvironment,
+          steps_single=5000,
+          steps_multi=0,
+          **env_kwargs)
 
     eval(MultiAgentEnvironment, num_games=10, render_mode="human", **env_kwargs)
